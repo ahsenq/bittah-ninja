e265e583ae1332ed2a961cda7880b05c8ae55b55
Alex Dauenhauer <alexdauenhauer17@gmail.com>
2019-09-21 10:02:13 -0500

    adding video slicer

---
+++ sliceVid.sh
@@ -0,0 +1,23 @@
+# get duration of video from input argument 1 (the path to the video)
+n=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1)
+
+# round duration of video
+n=$(echo "$n/1" | bc)
+
+# set parameters, extract filenames and paths, make a save directory
+start=0
+duration=6
+rootdir=$(echo $1 | cut -d"/" -f2)
+vid=$(echo $1 | cut -d"/" -f3)
+vid=$(echo $vid | cut -d"." -f1)
+savedir="$rootdir/$vid"
+mkdir $savedir
+
+# slice into 10 second slices
+while test $start -lt $n
+do
+    filename="${vid}_slice${start}.mp4"
+    filepath="$savedir/$filename"
+    ffmpeg -i $1 -ss $start -t $duration $filepath
+    start=$(($start+6))
+done

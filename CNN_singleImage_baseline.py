22a8855e354a7e99fdbe4286498d73750a89479f
Alex Dauenhauer <alexdauenhauer17@gmail.com>
2019-11-09 20:44:28 -0600

    updated singleImageScript

--- CNN_singleImage_baseline.py
+++ CNN_singleImage_baseline.py
@@ -1,9 +1,16 @@
 # %%
+# Only execute if running in container
+# !apt install libsm6 libxext6 libxrender-dev git -y
+# !pip3 install pandas scikit-learn opencv-python tqdm
+
+
+# %%
 import argparse
 import os
 import pickle
 import sys
 from collections import Counter
+from datetime import datetime
 
 import cv2
 import numpy as np
@@ -12,11 +19,17 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
-import keras
-from keras.layers import (Activation, Conv2D, Dense, Dropout, Flatten,
-                          MaxPooling2D)
-from keras.models import Model, Sequential
-from keras.utils import Sequence
+from tensorflow import keras
+from tensorflow.keras.layers import (Activation, Conv2D, Dense, Dropout, Flatten,
+                                     MaxPooling2D)
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.utils import Sequence
+
+
+# %%
+print(tf.__version__)
+print("GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
 
 # %%
 parser = argparse.ArgumentParser()
@@ -26,46 +39,46 @@ parser.add_argument('--batch_size', default=32, type=int)
 try:
     args = parser.parse_args()
 except:
-    args = parser.parse_args([])
+    # may need to modify vidpath depending on where you are running the script
+    #     args = parser.parse_args(['--vidpath=/tf/data/vids/scaled', '--batch_size=4'])
+    args = parser.parse_args(['--vidpath=/data/vids/scaled'])
+print(args.vidpath, args.epochs)
+
+
+# %%
+get_ipython().system('pwd')
+
 
 # %%
 # labelPath = 'bittah-ninja/first_1k_labeled_long_vids_removed.csv'
-labelPath = 'first_1k_labeled_long_vids_removed.csv'
+labelPath = 'week10_labeled.csv'
 # labelPath = 'full_labels.csv'
 df = pd.read_csv(labelPath)
+df.head()
+# %%
 new_files = []
 for file in df.clip_title:
     newfile = ''.join(file.split('.mp4')) + '.mp4'
     new_files.append(newfile)
 df['clip_title'] = new_files
-# df['label'] = df['class']
-# df.drop(columns=['class'], inplace=True)
+df['label'] = df['class']
+df.drop(columns=['class'], inplace=True)
 df.groupby('label').size()
-
 # %%
 df = df.loc[df.label != -1]
+df = df.loc[df.label != 99]
 df.groupby('label').size()
-# %%
-df.shape
+
 # %%
 # df['punch'] = (df.label != 0).astype('int')
 # df.groupby('punch').size()
-
 # %%
 vidPath = args.vidpath
 filenames = [f.split('.mp4')[0] + '_scaled.mp4' for f in df.clip_title]
 filenames = [os.path.join(vidPath, f) for f in filenames]
 # labels = df.punch.tolist()
 labels = df.label.tolist()
-
 # %%
-# def getMaxFrameCount(filenames):
-#     frameCount = []
-#     for file in tqdm(filenames):
-#         cap = cv2.VideoCapture(file)
-#         frameCount.append(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
-
-#     return max(frameCount)
 
 
 class DataGenerator(Sequence):
@@ -85,7 +98,7 @@ class DataGenerator(Sequence):
         self.n_channels = n_channels
 
     def __len__(self):
-        return np.ceil(len(self.filenames) / self.batch_size).astype(int)
+        return np.floor(len(self.filenames) / self.batch_size).astype(int)
 
     def __data_generation(self, idx_list):
         def getSingleFrame(filepath):
@@ -110,7 +123,7 @@ class DataGenerator(Sequence):
                       self.w,
                       self.h,
                       self.n_channels), dtype=np.float16)
-        y = np.empty((self.batch_size), dtype=np.float16)
+        y = np.empty(self.batch_size, dtype=np.float16)
         for i, idx in enumerate(idx_list):
             file = self.filenames[idx]
             frame = getSingleFrame(file)
@@ -119,32 +132,30 @@ class DataGenerator(Sequence):
                                   self.n_channels)
             x[i, ] = frame
             y[i, ] = self.labels[idx]
-            y = tf.keras.utils.to_categorical(y,
-                                              num_classes=len(set(self.labels)),
-                                              dtype='float16')
-        # print(x.shape, y.shape)
+        y = tf.keras.utils.to_categorical(
+            y, num_classes=len(set(self.labels)), dtype='float16')
         return x, y
-        # yield x, y
 
     def __getitem__(self, idx):
         batch = range(idx * self.batch_size, (idx + 1) * self.batch_size)
         x, y = self.__data_generation(batch)
         return x, y
-        # yield x, y
 
 
 # %%
 batch_size = args.batch_size
 labels_counts = Counter(labels)
 # TODO: Not sure if this should be 1 - x or x
-class_weight = {k:1-(v/len(labels)) for k,v in labels_counts.items()}
+class_weight = {k: 1 - (v / len(labels)) for k, v in labels_counts.items()}
+
 # %%
 x_train, x_test, y_train, y_test = train_test_split(
     filenames, labels, test_size=0.2)
 print(len(x_train), len(y_train))
 print(len(x_test), len(y_test))
+
+
 # %%
-# max_frame_count = getMaxFrameCount(filenames)
 train_generator = DataGenerator(x_train,
                                 y_train,
                                 batch_size)
@@ -152,16 +163,10 @@ test_generator = DataGenerator(x_test,
                                y_test,
                                batch_size)
 len(train_generator), len(test_generator)
-# %%
-# lab, batch = next(train_generator)
-# %%
-
-# %%
 
 # %%
 input_shape = (224, 224, 1)
 epochs = args.epochs
-# model = Sequential()
 inputs = keras.layers.Input(shape=input_shape, name='inputs')
 conv = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
 pool = MaxPooling2D(pool_size=(2, 2))(conv)
@@ -172,11 +177,12 @@ conv = Conv2D(32, (4, 4), activation='relu', padding='same')(pool)
 pool = MaxPooling2D(pool_size=(2, 2))(conv)
 conv = Conv2D(32, (4, 4), activation='relu', padding='same')(pool)
 pool = MaxPooling2D(pool_size=(2, 2))(conv)
-dropout = Dropout(0.25)(pool)
+dropout = Dropout(0.5)(pool)
 flat = Flatten()(dropout)
 dense = Dense(16, activation='relu')(flat)
 dropout = Dropout(0.25)(dense)
-outputs = Dense(len(set(labels)), activation='softmax', name='outputs')(dropout)
+outputs = Dense(len(set(labels)), activation='softmax',
+                name='outputs')(dropout)
 model = Model(inputs, outputs)
 
 model.compile(loss='categorical_crossentropy', optimizer='adam')
@@ -184,94 +190,24 @@ model.summary()
 
 
 # %%
-# inputs = keras.layers.Input(shape=input_shape, name='inputs')
-# conv2D = keras.layers.Conv2D(1, kernel_size=(3,3))
-# conv3 = Conv3D(4, (3, 3, 3), strides=(1, 1, 1), padding='same',
-#                data_format='channels_last', activation='relu')(inputs)
-# pool = keras.layers.MaxPooling3D(pool_size=(2, 2, 2),
-#                                  strides=(2, 2, 2), padding='same',
-#                                  data_format='channels_last')(conv3)
-# drop = Dropout(0.5)(pool)
-# flat = Flatten()(drop)
-# # model.add(Dense(128, activation='relu'))
-# # model.add(Dense(64, activation='relu'))
-# outputs = Dense(1, activation='sigmoid')(flat)
-# model = Model(inputs, outputs)
-
-
-# %%
-
-# %%
+es = keras.callbacks.EarlyStopping(
+    monitor='val_loss', patience=10, restore_best_weights=True)
 hist = model.fit_generator(generator=train_generator,
                            steps_per_epoch=(len(x_train) // batch_size),
-                           epochs=1,
+                           epochs=100,
                            verbose=1,
                            validation_data=test_generator,
                            validation_steps=(len(x_test) // batch_size),
-                           class_weight=class_weight,
-                           use_multiprocessing=True)
-
-# %%
-# Appendix
-# %%
-
-
-def getSingleFrame(filepath):
-    cap = cv2.VideoCapture(filepath)
-    vid = []
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.resize(gray, (h, w))
-            vid.append(gray)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
-    cap.release()
-    frame = np.random.choice(np.array(vid), 1)
-    return frame
+                           shuffle=False,
+                           # class_weight=class_weight,
+                           use_multiprocessing=False,
+                           callbacks=[es])
 
 
 # %%
-batch_size = 4
-w = 224
-h = 224
-n_channels = 1
-idx_list = range(10)
-x = np.empty((batch_size,
-              w,
-              h,
-              n_channels), dtype=np.float16)
-y = np.empty((batch_size), dtype=np.float16)
-for i, idx in enumerate(idx_list):
-    file = filenames[idx]
-    cap = cv2.VideoCapture(file)
-    vid = []
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.resize(gray, (h, w))
-            vid.append(gray)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
-    cap.release()
-    j = int(np.random.choice(len(vid), 1))
-    frame = vid[j]
-    # frame = getSingleFrame(file)
-    frame = frame.reshape(w,
-                          h,
-                          n_channels)
-    x[i, ] = frame
-    y[i, ] = labels[idx]
-
-# %%
-filenames2 = [os.path.join(vidPath, f) for f in os.listdir(vidPath)]
-[f for f in filenames if f in filenames2]
-
-# %%
-!pwd
+dt = datetime.now().strftime("%Y%m%d_%H%M%S")
+modelpath = '/data/models'
+os.makedirs(modelpath, exist_ok=True)
+model.save(os.path.join(modelpath, f'simpleCNN_{args.epochs}epochs_{dt}.h5'))
+with open(os.path.join(modelpath, f'simpleCNN_history_{args.epochs}epochs_{dt}.pickle'), 'wb') as f:
+    pickle.dump(hist.history, f, protocol=-1)
